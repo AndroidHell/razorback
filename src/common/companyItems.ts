233fc2a8eb8a5b07dd31a78022cf1426ff815e33
AndroidHell <ralkkai1337@gmail.com>
2024-08-19 10:57:19 -0500

    initial commit

---
+++ src/common/companyItems.ts
@@ -0,0 +1,6 @@
+export const brand = {
+  company: {
+    name: "Razorback",
+    tagline: "Company Tag Name",
+  },
+};
---
+++ src/components/headers/navigationItems.ts
@@ -0,0 +1,44 @@
+interface NavItem {
+  title: string;
+  path: string;
+  icon: string;
+  children?: { title: string; path: string }[];
+  target?: string; //add target: "_blank" to open in new tab
+}
+
+export const navigationItems: NavItem[] = [
+  {
+    title: "Home",
+    path: "/",
+    icon: "home",
+  },
+  {
+    title: "About Us",
+    path: "/about",
+    icon: "about",
+  },
+  {
+    title: "Projects",
+    path: "#",
+    icon: "projects",
+    children: [
+      { title: "Project 1", path: "/projects" },
+      { title: "Project 2", path: "/projects" },
+    ],
+  },
+  {
+    title: "Reviews",
+    path: "/reviews",
+    icon: "reviews",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+    icon: "blog",
+  },
+  // {
+  //   title: "Contact Us",
+  //   path: "/contact",
+  //   icon: "contactus",
+  // },
+];
